40d65a22f8c6b288bb263a10025a7e23701f73cf
korrieha <korrieha@discovery1.usc.edu>
2021-03-24 16:15:17 -0700

    updated clinic

--- R_code/clinic.R
+++ R_code/clinic.R
@@ -7,12 +7,14 @@ library(TCGAbiolinks)
 barcodes_clinic <- c("TCGA-BH-A0DG","TCGA-A2-A0YF","TCGA-AN-A04A","TCGA-AR-A1AP", "TCGA-A2-A0T3",
                      "TCGA-E2-A154", "TCGA-AO-A12F", "TCGA-A2-A0YM", "TCGA-BH-A0E0", "TCGA-AN-A0FL")
 
-install.packages("survival")
-install.packages("survminer")
-install.packages("arsenal")
+#install.packages("survival", repos = "http://cran.us.r-project.org")
+#install.packages("survminer", repos = "http://cran.us.r-project.org")
+#install.packages("arsenal", repos = "http://cran.us.r-project.org")
 library(survival)
 library(survminer)
 library(arsenal)
+
+#remove.packages("ggplot2")
 
 clin_query <- GDCquery(project = "TCGA-BRCA", data.category="Clinical", file.type="xml")
 GDCdownload( clin_query )
@@ -22,7 +24,7 @@ names(clinic)[names(clinic) == "days_to_last_followup"] = "days_to_last_follow_u
 age_clinical = clinic$age_at_initial_pathologic_diagnosis
 clinic$age_category = ifelse(age_clinical < 40, "Young", ifelse(age_clinical >= 60, "Old", "Mid"))
 
-install.packages("tableone")
+#install.packages("tableone", repos = "http://cran.us.r-project.org")
 library(tableone)
 clinic_summary <- CreateTableOne(data = clinic)
 subtypes <- TCGAquery_subtype(tumor = "BRCA")
@@ -35,4 +37,41 @@ table_arse <- tableby(age_category ~ (pathologic_stage)+ (mRNA.Clusters)+ (BRCA_
                       data = subtypes, numeric.test="kwt", cat.test="chisq",
                       numeric.stats = c("Nmiss", "meansd"), total=FALSE)
 df <- as.data.frame(summary(table_arse, text=TRUE, pfootnote=TRUE))
-write.csv(df,"data/tableby.csv", row.names=FALSE)
+write.csv(df,'summarize_by_age.csv', row.names=FALSE)
+
+
+###MAF###
+#BiocManager::install("maftools")
+library(maftools)
+#mutation <- GDCquery_Maf(tumor = "BRCA",save.csv=TRUE, pipeline="mutect2") #choose a pipeline
+# after running ^^, navigate to the saved csv file. Open the csv file with below Code
+# only need to query once. For repeating code, you can just read in the saved dataframe you created
+
+clinic$Overall_Survival_Status <- 1
+clinic$Overall_Survival_Status[which(clinic$vital_status != "Dead")] <- 0
+clinic$time <- clinic$days_to_death
+clinic$time[is.na(clinic$days_to_death)] <- clinic$days_to_last_follow_up[is.na(clinic$days_to_death)]
+colnames(clinic)[1] <- "Tumor_Sample_Barcode"
+
+my_maf <- data.table::fread("GDCdata/TCGA.BRCA.mutect.995c0111-d90b-4140-bee7-3845436c3b42.DR-10.0.somatic.maf.csv")
+
+maf <- read.maf(maf = my_maf, clinicalData = clinic, isTCGA = TRUE)
+
+pdf("mafSurvival.pdf")
+
+prog_geneset = survGroup(maf = maf, top = 20, geneSetSize = 2, time = "time", Status = "Overall_Survival_Status", verbose = FALSE)
+print(prog_geneset)
+
+mafSurvGroup(maf = maf, geneSet = c("TP53", "PIK3CA"), time = "time", Status = "Overall_Survival_Status")
+
+mafSurvGroup(maf = maf, geneSet = c("TP53", "MUC16"), time = "time", Status = "Overall_Survival_Status")
+
+mafSurvGroup(maf = maf, geneSet = c("MUC16", "PIK3CA"), time = "time", Status = "Overall_Survival_Status")
+
+mafSurvival(maf = maf, genes = 'TP53', time = 'time', Status = 'Overall_Survival_Status', isTCGA = TRUE)
+
+mafSurvival(maf = maf, genes = 'PIK3CA', time = 'time', Status = 'Overall_Survival_Status', isTCGA = TRUE)
+
+mafSurvival(maf = maf, genes = 'MUC16', time = 'time', Status = 'Overall_Survival_Status', isTCGA = TRUE)
+
+dev.off()
